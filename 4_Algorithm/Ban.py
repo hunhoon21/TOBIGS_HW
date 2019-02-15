2051c16bfea681f06891db2a7fb8aefb9218dd50
hunhoon21 <hunhoon21@gmail.com>
2019-02-16 02:30:55 +0900

    Algorithm 과제 준비

---
+++ 4_Algorithm/Ban.py
@@ -0,0 +1,8 @@
+class Ban:
+    def __init__(self, no=None):
+        self.no = no
+        self.student_list = []
+
+    def __str__(self):
+        pass
+    
---
+++ 4_Algorithm/Student.py
@@ -0,0 +1,13 @@
+class Student:
+    def __init__(self, id=None, name=None):
+        self.id = id
+        self.name = name
+
+    def __str__(self):
+        pass
+
+    def __lt__(self):
+        pass
+
+    def __eq__(self):
+        pass
