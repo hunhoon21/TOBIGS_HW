b4f95098e5733f1acff218047d5640d7f5361996
hunhoon21 <hunhoon21@gmail.com>
2019-02-16 02:32:21 +0900

    Algorithm 과제 준비

--- 4_Algorithm/Ban.py
+++ 4_Algorithm/Ban.py
@@ -5,4 +5,16 @@ class Ban:
 
     def __str__(self):
         pass
-    
+
+    def __lt__(self, other):
+        pass
+
+    def __eq__(self, other):
+        pass
+
+    def count_student(self):
+        """
+        해당 반에 속에있는 학생들 수
+        :return:
+        """
+        pass
